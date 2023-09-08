9ee7ca5a780fe36c8d2dd8c675713ea5f88aed9f
Victor Azevedo <victor.azevedo@mail.com>
2023-09-08 19:43:11 +0200

    Brought back CommonJS, Fixed exports

--- build.config.js
+++ build.config.js
@@ -2,6 +2,13 @@ import { buildSync } from 'esbuild'
 
 buildSync({
 	entryPoints: ['lib/index.js'],
+	outfile: 'dist/geopattern.cjs.js',
+	format: 'cjs',
+	bundle: true,
+})
+
+buildSync({
+	entryPoints: ['lib/index.js'],
 	outfile: 'dist/geopattern.esm.js',
 	format: 'esm',
 	bundle: true,
