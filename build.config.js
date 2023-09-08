930913203fb89717f96c48671ba00cf1ff7bdd09
Victor Azevedo <victor.azevedo@mail.com>
2023-09-08 11:35:15 +0200

    Renamed files to geopattern.*.js

--- build.config.js
+++ build.config.js
@@ -2,14 +2,14 @@ import { buildSync } from 'esbuild'
 
 buildSync({
 	entryPoints: ['lib/index.js'],
-	outfile: 'dist/index.esm.js',
+	outfile: 'dist/geopattern.esm.js',
 	format: 'esm',
 	bundle: true,
 })
 
 buildSync({
 	entryPoints: ['lib/index.js'],
-	outfile: 'dist/index.min.js',
+	outfile: 'dist/geopattern.min.js',
 	format: 'iife',
 	minify: true,
 	bundle: true,
